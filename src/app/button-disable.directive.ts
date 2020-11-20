1ed97e43ad3a2ae780c7abbbd7476a8420b5cb03
erzhukov <erzhukov@i-teco.ru>
2020-11-20 14:20:21 +0300

    task8

--- src/app/button-disable.directive.ts
+++ src/app/button-disable.directive.ts
@@ -3,27 +3,36 @@ import {
   ElementRef,
   HostBinding,
   Input,
+  OnInit,
   Renderer2,
 } from '@angular/core';
 
 @Directive({
   selector: '[buttonDisabled]',
 })
-export class ButtonDisableDirective {
+export class ButtonDisableDirective implements OnInit {
+  @Input() checked = true;
+  checkbox: Node;
+
   constructor(private elementRef: ElementRef, private renderer2: Renderer2) {
     this.checkbox = this.renderer2.createElement('input');
     this.renderer2.setAttribute(this.checkbox, 'type', 'checkbox');
     this.renderer2.listen(this.checkbox, 'click', this.onClickBox.bind(this));
     this.renderer2.appendChild(elementRef.nativeElement, this.checkbox);
   }
-  condition = false;
-  checkbox: Node;
+
+  ngOnInit(): void {
+    if (this.checked) {
+      this.renderer2.setAttribute(this.checkbox, 'checked', 'checked');
+    }
+  }
+
   @HostBinding('disabled')
   get disabled(): string {
-    return !this.condition ? 'disabled' : null;
+    return !this.checked ? 'disabled' : null;
   }
 
   onClickBox(): void {
-    this.condition = !this.condition;
+    this.checked = !this.checked;
   }
 }
